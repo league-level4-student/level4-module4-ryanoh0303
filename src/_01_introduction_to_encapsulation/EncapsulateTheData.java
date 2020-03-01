83b8c73f6c5da038da38fac2638aea0d7cc4c30f
League Student <league@iMac-28.attlocal.net>
2020-03-01 11:23:06 -0800

    Still working on Polymorphs

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -16,6 +16,7 @@ package _01_introduction_to_encapsulation;
  * */
 
 
+
 public class EncapsulateTheData {
 	//1. Encapsulate the member variables.
 	//   Add restrictions to the setters according to the comment.
@@ -23,12 +24,54 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
 	
 	public static void main(String[] args) {
 		
+	}
+	public int getitemsReceived() {
+		return itemsReceived;
+	}
+	public void setitemsReceived(int i) {
+		if(i<0) {
+			i=0;
+		}
+		itemsReceived = i;
+	}
+	public float getdegreesTurned() {
+		return degreesTurned;
+	}
+	public void setdegreesTurned(float x) throws Exception {
+		if(0.0<=x && x<=360) {
+			degreesTurned = x;
+		}
+		else {
+			throw new Exception();
+		}
+	}
+	public String getNomenclature() {
+		return nomenclature;
+	}
+	public void setNomenclature(String x)  throws Exception{
+		if(x.length()==0) {
+			throw new Exception();
+		}
+		else {
+			nomenclature = x;
+		}
+	}
+	public Object getmemberObj() {
+		return memberObj;
+	}
+	public void setmemberObj(Object x) throws Exception {
+		if(x instanceof String) {
+			throw new Exception();
+		}
+		else {
+			memberObj= x;
+		}
 	}
 }
